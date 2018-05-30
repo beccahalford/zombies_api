04fe241ce1ee8ff9e7584f3636c691a91362a930
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2018-05-30 13:12:21 +0100

    model setup

--- zombies/admin.py
+++ zombies/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from zombies.models import Map, Fact, Perk, Location, GobbleGum
+
+
+class FactInline(admin.TabularInline):
+    model = Fact
+
+
+@admin.register(Map)
+class MapAdmin(admin.ModelAdmin):
+    model = Map
+    inlines = [FactInline]
+    list_display = ['id', 'name']
+
+
+@admin.register(GobbleGum)
+class GobbleGumAdmin(admin.ModelAdmin):
+    model = GobbleGum
+    list_display = ['id', 'name', 'type']
--- zombies/models.py
+++ zombies/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Map(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField()
+    perks = models.ManyToManyField(through='app_name.PerkLocation')
+
+
+class Location(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    map = models.ForeignKey('app_name.Map')
+
+
+class Perk(models.Model):
+    name = models.CharField(choices=(
+        ('quick_revive', 'Quick Revive'),
+        ('juggernog', 'Juggernog'),
+        ('speed_cola', 'Speed Cola'),
+        ('double_tap', 'Double Tap')
+    ))
+    location = models.ForeignKey('app_name.Location')
+
+
+class PerkLocation(models.Model):
+    location = models.ForeignKey('app_name.Location')
+    perk = models.ForeignKey('app_name.Perk')
+
+
+class RandomFact(models.Model):
+    description = models.TextField()
+
+
+class Fact(models.Model):
+    description = models.TextField()
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+
+
+class GobbleGum(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    type = models.CharField(max_length=100)
