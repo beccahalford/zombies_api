1cfe26b8216e2263fe3c567e77a6d9fde0022782
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2018-05-31 14:03:08 +0100

    model refactor

--- zombies/admin.py
+++ zombies/admin.py
@@ -1,18 +1,50 @@
 from django.contrib import admin
 
-from zombies.models import Map, Fact, Perk, Location, GobbleGum
+from zombies.models import Fact, GobbleGum, Location, Map, Perk
 
 
 class FactInline(admin.TabularInline):
     model = Fact
 
 
+class LocationInline(admin.StackedInline):
+    model = Location
+    extra = 1
+    list_display = ['map', 'description']
+
+
+@admin.register(Perk)
+class PerkAdmin(admin.ModelAdmin):
+    model = Perk
+    inlines = [LocationInline]
+    list_display = ['id', 'name']
+
+
 @admin.register(Map)
 class MapAdmin(admin.ModelAdmin):
     model = Map
-    inlines = [FactInline]
     list_display = ['id', 'name']
 
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    model = Location
+    list_display = ['id', 'map', 'perk', 'description']
+
+
+# admin.site.register(Location)
+#
+# @admin.register(Map)
+# class MapAdmin(admin.ModelAdmin):
+#     model = Map
+#     inlines = [FactInline]
+#     list_display = ['id', 'name']
+#
+#
+# @admin.register(PerkLocation)
+# class PerkLocationAdmin(admin.ModelAdmin):
+#     model = PerkLocation
+#     list_display = ['map', 'perk', 'description']
 
 @admin.register(GobbleGum)
 class GobbleGumAdmin(admin.ModelAdmin):
--- zombies/models.py
+++ zombies/models.py
@@ -1,31 +1,53 @@
 from django.db import models
 
 
-class Map(models.Model):
+class Perk(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
-    name = models.CharField()
-    perks = models.ManyToManyField(through='app_name.PerkLocation')
+    name = models.CharField(max_length=100)
 
 
 class Location(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    map = models.ForeignKey('app_name.Map')
+    description = models.TextField(default='')
+    map = models.ForeignKey('zombies.Map', on_delete=models.CASCADE, default='')
+    perk = models.ForeignKey(Perk, on_delete=models.CASCADE, default='')
 
 
-class Perk(models.Model):
-    name = models.CharField(choices=(
-        ('quick_revive', 'Quick Revive'),
-        ('juggernog', 'Juggernog'),
-        ('speed_cola', 'Speed Cola'),
-        ('double_tap', 'Double Tap')
-    ))
-    location = models.ForeignKey('app_name.Location')
+class Map(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=100)
+    perks = models.ManyToManyField(Perk, blank=True)
 
-
-class PerkLocation(models.Model):
-    location = models.ForeignKey('app_name.Location')
-    perk = models.ForeignKey('app_name.Perk')
+#
+# class Map(models.Model):
+#     id = models.CharField(max_length=100, primary_key=True)
+#     name = models.CharField(max_length=200)
+#
+#
+# class Location(models.Model):
+#     name = models.CharField(max_length=200)
+#
+#
+# class Perk(models.Model):
+#     PERK_CHOICES = (('quick_revive',    'Quick Revive'),
+#         ('juggernog',       'Juggernog'),
+#         ('speed_cola',      'Speed Cola'),
+#         ('double_tap',      'Double Tap Root Beer'),
+#         ('phd_flopper',     'PhD Flopper'),
+#         ('stamin_up',       'Stamin-Up'),
+#         ('deadshot',        'Deadshot Daiquiri'),
+#         ('mule_kick',       'Mule Kick'),
+#         ('tombstone',       'Tombstone Soda'),
+#         ('whos_who',        'Who\'s Who'),
+#         ('electric_cherry', 'Electric Cherry'),
+#         ('vulture_aid',     'Vulture Aid Elixir'),
+#         ('widows_wine',     'Widow\'s Wine'))
+#     name = models.CharField(max_length=200, choices=PERK_CHOICES, default=PERK_CHOICES[0])
+#
+#
+# class PerkLocation(models.Model):
+#     perk = models.ForeignKey('zombies.Perk', on_delete=models.CASCADE)
+#     map = models.ForeignKey('zombies.Map', on_delete=models.CASCADE)
+#     description = models.TextField(default='None')
 
 
 class RandomFact(models.Model):
