cd8aa027f6849430ad55896c0397684c0749f3ea
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2018-06-12 11:13:42 +0100

    updated tests

--- zombies/tests.py
+++ zombies/tests.py
@@ -1,7 +1,7 @@
 import json
 
 from django.urls import reverse
-from rest_framework import status
+from datetime import datetime
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.authtoken.models import Token
 
@@ -10,26 +10,27 @@ from .models import Map
 
 
 class MapTests(APITestCase):
-    # def test_create_map(self):
-    #     """
-    #     Ensure we can create a new map object.
-    #     """
-    #     url = reverse('map-list')
-    #     data = {"map_id": "nacht", "name": "Nacht der Untoten", "release_date": "2018-06-04"}
-    #     response = self.client.post(url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(Map.objects.count(), 1)
-    #     self.assertEqual(Map.objects.get().name, 'Nacht der Untoten')
 
-    # def test_get_map(self):
-    #
+    def test_get_map(self):
+        map1 = Map(map_id='nacht', release_date='2018-06-04', name='Nacht der Untoten')
+        map2 = Map(map_id='shi_no_numa', release_date='2018-06-04', name='Shi no numa')
+
+        user = User.objects.create_superuser(username='test', email='', password='password')
+        map = Map.objects.create(map1, map2)
 
     def test_get_map_by_id(self):
         user = User.objects.create_superuser(username='test', email='', password='password')
+        map = Map.objects.create(
+            map_id='nacht',
+            release_date='2018-06-04',
+            name='Nacht der Untoten'
+        )
+
         token = Token.objects.create(user=user)
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-        response = self.client.get('/zombies/api/map/1/')
+        response = client.get(reverse('zombies:api:map-detail', args=(map.id,)))
+
         self.assertEqual(json.loads(response.content),
                          {'map_id': 'nacht',
                           'name': 'Nacht der Untoten',
