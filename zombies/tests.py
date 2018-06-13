262cc229f469271e33fd6b776275e17b929f4bbc
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2018-06-13 15:29:35 +0100

    added search filters

--- zombies/tests.py
+++ zombies/tests.py
@@ -1,7 +1,6 @@
 import json
 
 from django.urls import reverse
-from datetime import datetime
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.authtoken.models import Token
 
